e42a700b70e4048426b89af2216b73bb68430a16
Brian Tran <70680670+brian-trann@users.noreply.github.com>
2021-09-30 20:17:48 -0700

    init

---
+++ src/index.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer";
+import path from "path";
+
+import { writeFile, readdir, readFile } from "fs/promises";
+
+const configFiles = {};
+
+const configFolderPath = path.resolve(__dirname, "config");
+const gitignoreFolderPath = path.resolve(__dirname, "gitignore");
+
+async () => {
+  const files = await readdir(configFolderPath).catch(console.log);
+
+  for (let i of files) {
+    const nodeType = i.split(".")[1];
+    configFiles[nodeType];
+  }
+
+  const { technology, makeGitIgnore } = await inquirer.prompt([
+    {
+      type: "list",
+      message: "Pick the Node type",
+      name: "technology",
+      choices: Object.keys(configFiles),
+    },
+    {
+      type: "confirm",
+      message: "Do you want a gitignore",
+      name: "makeGitIgnore",
+      default: true,
+    },
+  ]);
+
+  const _tsconfig = await readFile(configFiles[technology]).catch(console.log);
+  const tsconfig = path.join(process.cwd(), "tsconfig.json");
+  const _gitignore = await readFile(configFiles[technology]).catch(console.log);
+  const gitignore = path.join(process.cwd(), ".gitignore");
+
+  await writeFile(tsconfig, _tsconfig.toString()).catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
+
+  if (makeGitIgnore) {
+    await writeFile(gitignore, _gitignore).catch((e) => {
+      console.log(e);
+      process.exit(1);
+    });
+  }
+
+  console.log("tsconfig.json created");
+};
